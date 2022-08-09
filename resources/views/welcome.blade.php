493bf26b435e2c25972bc708c2f627082436f44b
Luciano Quintana <lquintana@honorlock.com>
2022-08-09 12:48:46 -0500

    Adding magic number

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,6 +32,9 @@
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
                         @endif
+                        @if ('testing' === 5)
+                            <b>Magic Number not allowed </b>
+                        @endif
                     @endauth
                 </div>
             @endif
