0fff2b4b72044a9009eb73a7bf772665829cda82
Luciano Quintana <lquintana@honorlock.com>
2022-08-09 13:24:16 -0500

    Changing to on push for faster testing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('lucho-test', function(){
+    $luchoTest = 'someText';
+    if($luchoTest === 3){
+        echo "Magic Numbers not allowed";
+    }
+});
