f19fa74cb3af606811593de24858dffe46ffd2c1
Luciano Quintana <lquintana@honorlock.com>
2022-08-09 15:15:02 -0500

    Adding magic number

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('lucho-test', function(){
+    $luchoTest = 'someText';
+    if($luchoTest === 4){
+        echo "Magic Numbers not allowed";
+    }
+});
+
 
